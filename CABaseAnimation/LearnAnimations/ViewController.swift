e022b1773bdbe49256797d3fbf4b4e24d558f904
ZhiHua Shen <zhcoder2011@gmail.com>
2017-07-17 10:55:14 +0800

    https://github.com/ZHDeveloper/LearnAnimations.git

---
+++ CABaseAnimation/LearnAnimations/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LearnAnimations
+//
+//  Created by ZhiHua Shen on 2017/7/14.
+//  Copyright © 2017年 ZhiHua Shen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var colorView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+    
+}
+
